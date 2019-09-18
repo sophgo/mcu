b95ac6aa173ee372bd4760675ceb16f5b4f3dbaf
taiqiang.cao <taiqiang.cao@bitmain.com>
2019-09-18 10:24:46 +0800

    [stm32_bl] fix cannot detect i2c slave when MCU in bootloader mode BUG
    Change-Id: I60d70d959ba734533432e9895a49c0897452a889

--- stm32_bootloader/Src/i2c.c
+++ stm32_bootloader/Src/i2c.c
@@ -36,9 +36,9 @@ void MX_I2C1_Init(void)
   hi2c1.Init.Timing = 0x00000708;
   hi2c1.Init.OwnAddress1 = 112;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_ENABLE;
+  hi2c1.Init.OwnAddress2 = 112;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_MASK04;
   hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
   hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
   if (HAL_I2C_Init(&hi2c1) != HAL_OK)
