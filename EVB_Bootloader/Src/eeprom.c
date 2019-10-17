c1c61c0e84bb154c4018e4ea63a792c9505e6170
taiqiang.cao <taiqiang.cao@bitmain.com>
2019-10-17 11:23:33 +0800

    [EVB] evb bootloader init
    Change-Id: Idc03070b4fd01600be491852750128ddc770cba9

---
+++ EVB_Bootloader/Src/eeprom.c
@@ -0,0 +1,94 @@
+/*
+ * eeprom.c
+ *
+ *  Created on: 2019Äê4ÔÂ10ÈÕ
+ *      Author: taiqiang.cao
+ */
+
+#include "eeprom.h"
+
+/*
+ * EEPROM read data by Bytes
+ */
+void EEPROM_ReadBytes(uint16_t Addr, uint8_t *Buffer, uint16_t Length)
+{
+	uint8_t *wAddr;
+	wAddr = (uint8_t *)(EEPROM_BASE_ADDR + Addr);
+	while(Length--){
+		*Buffer++ = *wAddr++;
+	}
+}
+
+
+/*
+ * EEPROM read data by Words
+ */
+void EEPROM_ReadWords(uint16_t Addr, uint16_t *Buffer, uint16_t Length)
+{
+	uint32_t *wAddr;
+	wAddr = (uint32_t *)(EEPROM_BASE_ADDR + Addr);
+	while(Length--){
+		*Buffer++ = *wAddr++;
+	}
+}
+
+/*
+ * EEPROM write data by Bytes
+ */
+void EEPROM_WriteBytes(uint16_t Addr, uint8_t *Buffer, uint16_t Length)
+{
+	uint8_t *wAddr;
+	wAddr = (uint8_t *)(EEPROM_BASE_ADDR + Addr);
+	DIS_INT
+	if(FLASH->PECR&FLASH_PECR_PELOCK) {
+		FLASH->PEKEYR = PEKEY1;//unlock
+		FLASH->PEKEYR = PEKEY2;
+	}
+	while(FLASH->PECR&FLASH_PECR_PELOCK);
+	FLASH->PECR |= FLASH_PECR_FIX;	//Fixed Time Data write for Word/Half Word/Byte programming
+	while(Length--){
+		*wAddr++ = *Buffer++;
+		while(FLASH->SR&FLASH_SR_BSY);
+	}
+	FLASH->PECR|=FLASH_PECR_PELOCK;
+	EN_INT
+}
+
+/*
+ * EEPROM write data by Words
+ */
+void EEPROM_WriteWords(uint16_t Addr, uint16_t *Buffer, uint16_t Length)
+{
+	uint32_t *wAddr;
+	wAddr = (uint32_t *)(EEPROM_BASE_ADDR + Addr);
+	DIS_INT
+	if(FLASH->PECR&FLASH_PECR_PELOCK) {
+		FLASH->PEKEYR = PEKEY1;//unlock
+		FLASH->PEKEYR = PEKEY2;
+	}
+	while(FLASH->PECR&FLASH_PECR_PELOCK);
+	FLASH->PECR |= FLASH_PECR_FIX;	// Fixed Time Data write for Word/Half Word/Byte programming
+	while(Length--){
+		*wAddr++=*Buffer++;
+		while(FLASH->SR&FLASH_SR_BSY);
+	}
+	FLASH->PECR|=FLASH_PECR_PELOCK;
+	EN_INT
+}
+
+void EEPROM_Write(uint32_t Addr, uint32_t writeFlashData)
+{
+	HAL_FLASH_Unlock();
+	FLASH_EraseInitTypeDef f;
+	f.TypeErase = FLASH_TYPEERASE_PAGES;
+	f.PageAddress = Addr;
+	f.NbPages = 1;
+
+	uint32_t PageError = 0;
+
+	HAL_FLASHEx_Erase(&f, &PageError);
+
+	HAL_FLASH_Program(TYPEPROGRAM_WORD, Addr, writeFlashData);
+
+	HAL_FLASH_Lock();
+}
---
+++ EVB_Bootloader/Src/i2c_slave.c
@@ -0,0 +1,332 @@
+#include "i2c_slave.h"
+#include "flash_if.h"
+
+/** Declarations **/
+static HAL_StatusTypeDef I2C_SlaveISR(I2C_HandleTypeDef *hi2c,
+		uint32_t ITFlags, uint32_t ITSources);
+static void I2C_AddrMatch(I2C_HandleTypeDef *hi2c);
+static void I2C_SlaveProcessDone(I2C_HandleTypeDef *hi2c, uint8_t addr);
+static void I2C_EnableIRQ(I2C_HandleTypeDef *hi2c, uint16_t InterruptRequest);
+static void I2C_DisableIRQ(I2C_HandleTypeDef *hi2c, uint16_t InterruptRequest);
+static void I2C_FlushTXDR(I2C_HandleTypeDef *hi2c);
+
+#define I2C_XFER_TX_IT          (0x00000001U)
+#define I2C_XFER_RX_IT          (0x00000002U)
+#define I2C_XFER_LISTEN_IT      (0x00000004U)
+#define I2C_NO_OPTION_FRAME     (0xFFFF0000U)
+#define I2C_XFER_CPLT_IT        (0x00000012U)
+
+/** Source Code **/
+//register slave initial
+HAL_StatusTypeDef I2C_SlaveInit(I2C_HandleTypeDef *hi2c, uint8_t *pData,
+		uint8_t size) {
+	/* Process Locked */
+	__HAL_LOCK(hi2c);
+	if ((uint32_t) hi2c->State == HAL_I2C_STATE_READY) {
+
+	hi2c->State = HAL_I2C_STATE_LISTEN;
+	hi2c->Mode = HAL_I2C_MODE_SLAVE;
+	hi2c->ErrorCode = HAL_I2C_ERROR_NONE;
+
+	/* Enable Address Acknowledge */
+	hi2c->Instance->CR2 &= ~I2C_CR2_NACK;
+
+	/* Prepare transfer parameters */
+	hi2c->pBuffPtr = pData;
+	hi2c->XferCount = 0xFFFF;
+	hi2c->XferSize = size;
+	hi2c->XferOptions = I2C_NO_OPTION_FRAME;
+	hi2c->XferISR = I2C_SlaveISR;
+	} else {
+		return HAL_BUSY;
+	}
+	/* Process Unlocked */
+	__HAL_UNLOCK(hi2c);
+
+	/* Note : The I2C interrupts must be enabled after unlocking current process
+	 to avoid the risk of I2C interrupt handle execution before current
+	 process unlock */
+	/* REnable ADDR interrupt */
+	I2C_EnableIRQ(hi2c, I2C_XFER_RX_IT | I2C_XFER_TX_IT | I2C_XFER_LISTEN_IT);
+
+	return HAL_OK;
+}
+
+HAL_StatusTypeDef I2C_SlaveAbort(I2C_HandleTypeDef *hi2c) {
+	/* Disable all interrupts */
+	I2C_DisableIRQ(hi2c, I2C_XFER_LISTEN_IT | I2C_XFER_TX_IT | I2C_XFER_RX_IT);
+	/* Disable Address Acknowledge */
+	hi2c->Instance->CR2 |= I2C_CR2_NACK;
+	/* Clear Configuration Register 2 */
+	I2C_RESET_CR2(hi2c);
+	/* Flush TX register */
+	I2C_FlushTXDR(hi2c);
+	/* Reset I2C handle */
+	hi2c->State = HAL_I2C_STATE_READY;
+	hi2c->Mode = HAL_I2C_MODE_NONE;
+	hi2c->ErrorCode = HAL_I2C_ERROR_NONE;
+	hi2c->pBuffPtr = NULL;
+	hi2c->XferCount = 0;
+	hi2c->XferSize = 0;
+	hi2c->XferOptions = I2C_NO_OPTION_FRAME;
+	hi2c->XferISR = NULL;
+
+	__HAL_UNLOCK(hi2c);
+	return HAL_OK;
+}
+
+/* Packet structure defines */
+#define PACKET_HEADER_SIZE      ((uint32_t)3)
+#define PACKET_DATA_INDEX       ((uint32_t)4)
+#define PACKET_START_INDEX      ((uint32_t)1)
+#define PACKET_NUMBER_INDEX     ((uint32_t)2)
+#define PACKET_CNUMBER_INDEX    ((uint32_t)3)
+#define PACKET_TRAILER_SIZE     ((uint32_t)2)
+#define PACKET_OVERHEAD_SIZE    (PACKET_HEADER_SIZE + PACKET_TRAILER_SIZE - 1)
+#define PACKET_SIZE             ((uint32_t)128)
+#define PACKET_1K_SIZE          ((uint32_t)1024)
+
+/* @note ATTENTION - please keep this variable 32bit alligned */
+uint8_t aPacketData[PACKET_1K_SIZE + PACKET_DATA_INDEX + PACKET_TRAILER_SIZE];
+
+static uint8_t addr;
+uint32_t ramsource;
+uint32_t packet_length = 8;
+uint8_t flash_write = 0;
+static uint8_t cnt_update = 0;
+static uint8_t package_cnt = 0;
+uint8_t buf_update[FLASH_PAGE_SIZE + 4];
+static uint8_t update_flag = 0;
+
+/* Initialize flashdestination variable */
+uint32_t flashdestination = 0x8008000;
+uint8_t buf = 0;
+extern volatile uint8_t reg[];
+
+
+static HAL_StatusTypeDef I2C_SlaveISR(I2C_HandleTypeDef *hi2c,
+		uint32_t ITFlags, uint32_t ITSources) {
+
+	/* Process locked */
+	__HAL_LOCK(hi2c);
+
+	if ((I2C_CHECK_FLAG(ITFlags, I2C_FLAG_AF) != RESET)
+			&& (I2C_CHECK_IT_SOURCE(ITSources, I2C_IT_NACKI) != RESET)) {
+		/* Check that I2C transfer finished */
+		/* if yes, normal use case, a NACK is sent by the MASTER when Transfer is finished */
+		__HAL_I2C_CLEAR_FLAG(hi2c, I2C_FLAG_AF);
+		I2C_SlaveProcessDone(hi2c, addr);
+	} else if ((I2C_CHECK_FLAG(ITFlags, I2C_FLAG_ADDR) != RESET)
+				&& (I2C_CHECK_IT_SOURCE(ITSources, I2C_IT_ADDRI) != RESET)) {
+			I2C_AddrMatch(hi2c);
+
+	} else if ((I2C_CHECK_FLAG(ITFlags, I2C_FLAG_RXNE) != RESET)
+			&& (I2C_CHECK_IT_SOURCE(ITSources, I2C_IT_RXI) != RESET)) {
+		/* incoming bytes */
+		ITFlags &= ~I2C_FLAG_RXNE;
+		/* Read data from RXDR */
+		buf = (uint8_t) hi2c->Instance->RXDR;
+		if (hi2c->XferCount == 0xFFFF && buf <= hi2c->XferSize && (update_flag == 0)) { // first byte, reg addr
+		/* Move the buffer ptr */
+			addr = buf;
+			hi2c->pBuffPtr += addr;
+			hi2c->XferCount = addr;
+			package_cnt = 0;
+		} else if (hi2c->XferCount != 0xFFFF
+				/*hi2c->XferCount < hi2c->XferSize*/&& (update_flag == 0)){// write reg
+				/* Increment Buffer pointer */
+			*hi2c->pBuffPtr = buf;
+			hi2c->pBuffPtr++;
+			hi2c->XferCount++;
+
+			if (hi2c->State != HAL_I2C_STATE_BUSY_TX_LISTEN)
+				hi2c->State = HAL_I2C_STATE_BUSY_RX_LISTEN;
+//		} else { // wrong reg addr or out of bound
+//			__HAL_I2C_GENERATE_NACK(hi2c);
+		}
+
+		if (buf == 0x42) {
+			buf_update[cnt_update] = buf;
+			cnt_update++;
+		} else if ((buf == 0x4d) && (cnt_update == 1)) {
+			buf_update[cnt_update] = buf;
+			cnt_update++;
+		} else if ((buf == 0x43) && (cnt_update == 2)){
+			buf_update[cnt_update] = buf;
+			cnt_update++;
+		} else if ((buf == 0x55) && (cnt_update == 3)){
+			buf_update[cnt_update] = buf;
+			cnt_update++;
+			update_flag = 1;
+		} else if ((cnt_update > 3) && (cnt_update < 132)) {
+			buf_update[cnt_update] = buf;
+			cnt_update++;
+		}
+
+		if (cnt_update == 132) {
+			STMFLASH_Write_Page(flashdestination + (package_cnt*128), buf_update + 4, 128);
+			cnt_update = 0;
+			update_flag = 0;
+			package_cnt++;
+		}
+
+	} else if ((I2C_CHECK_FLAG(ITFlags, I2C_FLAG_TXIS) != RESET)
+			&& (I2C_CHECK_IT_SOURCE(ITSources, I2C_IT_TXI) != RESET)) {
+		/* incoming bytes */
+		ITFlags &= ~I2C_FLAG_TXIS;
+		if (hi2c->XferCount == 0xFFFF)
+			hi2c->XferCount = 0; // start from beginning
+		if (1/*hi2c->XferCount < hi2c->XferSize*/) { // or a specific addr
+			hi2c->Instance->TXDR = *hi2c->pBuffPtr;
+			/* Increment Buffer pointer */
+			hi2c->pBuffPtr++;
+			hi2c->XferCount++;
+			hi2c->State = HAL_I2C_STATE_BUSY_TX_LISTEN;
+//		} else { // out of bound or no reg selected
+//			__HAL_I2C_GENERATE_NACK(hi2c);
+		}
+	}
+
+	/* Check if STOPF is set */
+	if ((I2C_CHECK_FLAG(ITFlags, I2C_FLAG_STOPF) != RESET)
+			&& (I2C_CHECK_IT_SOURCE(ITSources, I2C_IT_STOPI) != RESET)) {
+		/* Call I2C Slave complete process */
+//		I2C_SlaveProcessDone(hi2c, addr);
+		/* Clear STOP Flag */
+		__HAL_I2C_CLEAR_FLAG(hi2c, I2C_FLAG_STOPF);
+	}
+
+	/* Process Unlocked */
+	__HAL_UNLOCK(hi2c);
+
+	return HAL_OK;
+}
+
+static void I2C_AddrMatch(I2C_HandleTypeDef *hi2c) {
+	__HAL_I2C_CLEAR_FLAG(hi2c, I2C_FLAG_ADDR);
+	__HAL_UNLOCK(hi2c);
+	I2C_AddrCallback(hi2c);
+
+}
+
+static void I2C_SlaveProcessDone(I2C_HandleTypeDef *hi2c, uint8_t addr) {
+	/* Reset handle */
+//	if (update_flag == 0) {
+		hi2c->pBuffPtr -= hi2c->XferCount;
+		hi2c->XferCount = 0xFFFF;
+//	}
+	I2C_FlushTXDR(hi2c);
+	__HAL_UNLOCK(hi2c);
+
+	if (hi2c->State == HAL_I2C_STATE_BUSY_TX_LISTEN) {
+		I2C_SlaveTxCallback(hi2c, addr);
+	} else if (hi2c->State == HAL_I2C_STATE_BUSY_RX_LISTEN) {
+		I2C_SlaveRxCallback(hi2c, addr);
+	}
+	hi2c->State = HAL_I2C_STATE_LISTEN;
+
+
+}
+
+static void I2C_EnableIRQ(I2C_HandleTypeDef *hi2c, uint16_t InterruptRequest) {
+	uint32_t tmpisr = 0U;
+	{
+		if ((InterruptRequest & I2C_XFER_LISTEN_IT) == I2C_XFER_LISTEN_IT) {
+			/* Enable ERR, STOP, NACK, and ADDR interrupts */
+			tmpisr |= I2C_IT_ADDRI | I2C_IT_STOPI | I2C_IT_NACKI | I2C_IT_ERRI;
+		}
+
+		if ((InterruptRequest & I2C_XFER_TX_IT) == I2C_XFER_TX_IT) {
+			/* Enable ERR, TC, STOP, NACK and RXI interrupts */
+			tmpisr |= I2C_IT_ERRI | I2C_IT_TCI | I2C_IT_STOPI | I2C_IT_NACKI
+					| I2C_IT_TXI;
+		}
+
+		if ((InterruptRequest & I2C_XFER_RX_IT) == I2C_XFER_RX_IT) {
+			/* Enable ERR, TC, STOP, NACK and TXI interrupts */
+			tmpisr |= I2C_IT_ERRI | I2C_IT_TCI | I2C_IT_STOPI | I2C_IT_NACKI
+					| I2C_IT_RXI;
+		}
+
+		if ((InterruptRequest & I2C_XFER_CPLT_IT) == I2C_XFER_CPLT_IT) {
+			/* Enable STOP interrupts */
+			tmpisr |= I2C_IT_STOPI;
+		}
+	}
+
+	/* Enable interrupts only at the end */
+	/* to avoid the risk of I2C interrupt handle execution before */
+	/* all interrupts requested done */
+	__HAL_I2C_ENABLE_IT(hi2c, tmpisr);
+}
+
+static void I2C_DisableIRQ(I2C_HandleTypeDef *hi2c, uint16_t InterruptRequest) {
+	uint32_t tmpisr = 0U;
+
+	if ((InterruptRequest & I2C_XFER_TX_IT) == I2C_XFER_TX_IT) {
+		/* Disable TC and TXI interrupts */
+		tmpisr |= I2C_IT_TCI | I2C_IT_TXI;
+
+		if (((uint32_t) hi2c->State & (uint32_t) HAL_I2C_STATE_LISTEN)
+				!= (uint32_t) HAL_I2C_STATE_LISTEN) {
+			/* Disable NACK and STOP interrupts */
+			tmpisr |= I2C_IT_STOPI | I2C_IT_NACKI | I2C_IT_ERRI;
+		}
+	}
+
+	if ((InterruptRequest & I2C_XFER_RX_IT) == I2C_XFER_RX_IT) {
+		/* Disable TC and RXI interrupts */
+		tmpisr |= I2C_IT_TCI | I2C_IT_RXI;
+
+		if (((uint32_t) hi2c->State & (uint32_t) HAL_I2C_STATE_LISTEN)
+				!= (uint32_t) HAL_I2C_STATE_LISTEN) {
+			/* Disable NACK and STOP interrupts */
+			tmpisr |= I2C_IT_STOPI | I2C_IT_NACKI | I2C_IT_ERRI;
+		}
+	}
+
+	if ((InterruptRequest & I2C_XFER_LISTEN_IT) == I2C_XFER_LISTEN_IT) {
+		/* Disable ADDR, NACK and STOP interrupts */
+		tmpisr |= I2C_IT_ADDRI | I2C_IT_STOPI | I2C_IT_NACKI | I2C_IT_ERRI;
+	}
+
+	if ((InterruptRequest & I2C_XFER_CPLT_IT) == I2C_XFER_CPLT_IT) {
+		/* Enable STOP interrupts */
+		tmpisr |= I2C_IT_STOPI;
+	}
+
+	/* Disable interrupts only at the end */
+	/* to avoid a breaking situation like at "t" time */
+	/* all disable interrupts request are not done */
+	__HAL_I2C_DISABLE_IT(hi2c, tmpisr);
+}
+
+static void I2C_FlushTXDR(I2C_HandleTypeDef *hi2c) {
+	/* If a pending TXIS flag is set */
+	/* Write a dummy data in TXDR to clear it */
+	if (__HAL_I2C_GET_FLAG(hi2c, I2C_FLAG_TXIS) != RESET) {
+		hi2c->Instance->TXDR = 0x00U;
+	}
+
+	/* Flush TX register if not empty */
+	if (__HAL_I2C_GET_FLAG(hi2c, I2C_FLAG_TXE) == RESET) {
+		__HAL_I2C_CLEAR_FLAG(hi2c, I2C_FLAG_TXE);
+	}
+}
+
+__weak void I2C_AddrCallback(I2C_HandleTypeDef *hi2c) {
+	UNUSED(hi2c);
+}
+__weak void I2C_SlaveTxCallback(I2C_HandleTypeDef *hi2c, uint8_t addr) {
+	UNUSED(hi2c);
+}
+__weak void I2C_SlaveRxCallback(I2C_HandleTypeDef *hi2c, uint8_t addr) {
+	UNUSED(hi2c);
+//	ramsource = (uint32_t) & aPacketData[PACKET_DATA_INDEX];
+//	ramsource = 8;
+//	if (FLASH_If_Write(flashdestination, (uint32_t*) ramsource, packet_length/4) != FLASHIF_OK) {
+//	//if (FLASH_If_Write(flashdestination, (uint32_t*) buf, packet_length/4) != FLASHIF_OK) {
+//		while(1);
+//	}
+}
+
