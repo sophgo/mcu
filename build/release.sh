f7dce6a8eaa11f76766d2ad2ab4d01f7c64fade0
Chao Wei <chao.wei@bitmain.com>
2020-04-15 18:28:29 +0800

    add auto release script
    Change-Id: I2acef7826f55790a297bdef0342a4c8405e952b9
    
    Former-commit-id: 4ad1a4609eff73e263b2920f6e9d2db026f72913

---
+++ build/release.sh
@@ -0,0 +1,33 @@
+list='bm1684evb sa5 sc5h sc5h3 sm5pcie'
+
+unset rm
+
+function clean()
+{
+    for dir in $list; do
+        rm -rf $dir
+    done
+}
+
+function build()
+{
+    clean
+
+    # build libopencm3 first
+    pushd ../libopencm3
+        make clean && make
+    popd
+
+    date=`date '+%Y-%m-%d-%H-%M-%S'`
+    set -e
+    for proj in $list; do
+        make PROJ=$proj
+    done
+
+    for proj in $list; do
+        cp $proj/$proj-mcu.bin $proj/$proj-mcu-$date.bin
+        md5sum $proj/$proj-mcu-$date.bin
+    done
+}
+
+build
