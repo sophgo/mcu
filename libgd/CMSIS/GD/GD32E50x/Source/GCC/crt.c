5b7fbbed2d56bd13160015c98dc2f72cb0fe01b9
Chao Wei <chao.wei@bitmain.com>
2021-05-08 14:08:14 +0800

    GD32 firmware library
    Change-Id: Ifa90c707b896c6a47356226b1653e7fb3a710c2c
    
    Former-commit-id: 0209b0922b5f3161b1c4a699959904f4ab9f40b0

---
+++ libgd/CMSIS/GD/GD32E50x/Source/GCC/crt.c
@@ -0,0 +1,38 @@
+/* setup c runtime */
+
+#include <stdint.h>
+
+extern uint32_t __ld_data_load_start[0], __ld_data_load_end[0];
+extern uint32_t __ld_data_start[0], __ld_data_end[0];
+extern uint32_t __ld_bss_start[0], __ld_bss_end[0];
+extern void SystemInit(void);
+extern void main(void);
+
+void crt_init(void)
+{
+	/* stack is ready after chip init */
+	/* copy data */
+	uint32_t *src, *dst;
+
+	src = __ld_data_load_start;
+	dst = __ld_data_start;
+
+	while (dst != __ld_data_end) {
+		*dst = *src;
+		++dst;
+		++src;
+	}
+
+	/* zero bss */
+	for (dst = __ld_bss_start; dst != __ld_bss_end; ++dst)
+		*dst = 0;
+}
+
+void Reset_Handler(void)
+{
+	SystemInit();
+	crt_init();
+	main();
+	while (1)
+		;
+}
