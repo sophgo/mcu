be33ebf89dfa87ea1bea03816f1be606aaf6f3b6
chunzhi.lin <chunzhi.lin@sophgo.com>
2022-08-19 09:50:39 +0800

    add checkport for 2042
    Change-Id: I729b14c49a2c0baa3d54cab8ffdcef23975e6a31
    
    Former-commit-id: 3a1413d6a9c642070c458f8876b24c36e51a0fd4

--- SG2042EVB/board_power.c
+++ SG2042EVB/board_power.c
@@ -5,79 +5,98 @@
 #include <common.h>
 #include <stdlib.h>
 
-struct power_node const board_power_nodes[16] = {
+struct power_node const board_power_nodes[20] = {
 
-	{"EN_VDDC_B12", NODE_TYPE_ENABLE, 1000,
+	{"EN_VDD_3V3", NODE_TYPE_ENABLE, 1000,
 	 {(unsigned long)EN_VDD_3V3_PORT, (unsigned long)EN_VDD_3V3_PIN},
 	 },
 
-	{"EN_VDDC_B34", NODE_TYPE_ENABLE, 1000,
+	{"EN_VDDIO18", NODE_TYPE_ENABLE, 1000,
 	 {(unsigned long)EN_VDDIO18_PORT, (unsigned long)EN_VDDIO18_PIN},
 	 },
 
-	{"EN_VDDC_B56", NODE_TYPE_ENABLE, 1000,
+	{"EN_VDDC", NODE_TYPE_ENABLE, 1000,
 	 {(unsigned long)EN_VDDC_PORT, (unsigned long)EN_VDDC_PIN},
 	 },
 
-	{"EN_VDDC_B78", NODE_TYPE_ENABLE, 1000,
+	{"PG_VDDC", NODE_TYPE_CHECK, 0,
+	 {(unsigned long)PG_VDDC_PORT, (unsigned long)PG_VDDC_PIN},
+	 },
+
+	{"EN_VDDIO33", NODE_TYPE_ENABLE, 1000,
 	 {(unsigned long)EN_VDDIO33_PORT, (unsigned long)EN_VDDIO33_PIN},
 	 },
 
-	{"EN_VDDIO33_B12", NODE_TYPE_ENABLE, 0,
+	{"EN_DDR_VDD_0V8", NODE_TYPE_ENABLE, 0,
 	 {(unsigned long)EN_DDR_VDD_0V8_PORT,
 	  (unsigned long)EN_DDR_VDD_0V8_PIN},
 	 },
 
-	{"EN_VDDIO33_B34", NODE_TYPE_ENABLE, 0,
+	{"EN_VDD_PCIE_D_0V8", NODE_TYPE_ENABLE, 0,
 	 {(unsigned long)EN_VDD_PCIE_D_0V8_PORT,
 	  (unsigned long)EN_VDD_PCIE_D_0V8_PIN},
 	 },
 
-	{"EN_VDDIO33_B56", NODE_TYPE_ENABLE, 0,
+	{"EN_VDD_PLL_0V8", NODE_TYPE_ENABLE, 0,
 	 {(unsigned long)EN_VDD_PLL_0V8_PORT,
 	  (unsigned long)EN_VDD_PLL_0V8_PIN},
 	 },
 
-	{"EN_VDDIO33_B78", NODE_TYPE_ENABLE, 1000,
+	{"PG_DDR_VDD_0V8", NODE_TYPE_CHECK, 0,
+	 {(unsigned long)PG_DDR_VDD_0V8_PORT,
+	  (unsigned long)PG_DDR_VDD_0V8_PIN},
+	 },
+
+	{"EN_VDD_PCIE_H_1V8", NODE_TYPE_ENABLE, 1000,
 	 {(unsigned long)EN_VDD_PCIE_H_1V8_PORT,
 	  (unsigned long)EN_VDD_PCIE_H_1V8_PIN},
 	 },
 
-	{"EN_VDD_PHY_B12", NODE_TYPE_ENABLE, 0,
+	{"EN_DDR01_VPP_2V5", NODE_TYPE_ENABLE, 0,
 	 {(unsigned long)EN_DDR01_VPP_2V5_PORT,
 	  (unsigned long)EN_DDR01_VPP_2V5_PIN},
 	 },
 
-	{"EN_VDD_PHY_B34", NODE_TYPE_ENABLE, 1000,
+	{"EN_DDR23_VPP_2V5", NODE_TYPE_ENABLE, 1000,
 	 {(unsigned long)EN_DDR23_VPP_2V5_PORT,
 	  (unsigned long)EN_DDR23_VPP_2V5_PIN},
 	 },
 
-	{"EN_VDD_PHY_B56", NODE_TYPE_ENABLE, 0,
+	{"EN_DDR01_VDDQ_1V2", NODE_TYPE_ENABLE, 0,
 	 {(unsigned long)EN_DDR01_VDDQ_1V2_PORT,
 	  (unsigned long)EN_DDR01_VDDQ_1V2_PIN},
 	 },
 
-	{"EN_VDD_PHY_B78", NODE_TYPE_ENABLE, 1000,
+	{"EN_DDR23_VDDQ_1V2", NODE_TYPE_ENABLE, 1000,
 	 {(unsigned long)EN_DDR23_VDDQ_1V2_PORT,
 	  (unsigned long)EN_DDR23_VDDQ_1V2_PIN},
 	 },
 
-	{"P08_PG_B12", NODE_TYPE_ENABLE, 0,
+	{"PG_DDR01_VDDQ_1V2", NODE_TYPE_CHECK, 0,
+	 {(unsigned long)PG_DDR01_VDDQ_1V2_PORT,
+	  (unsigned long)PG_DDR01_VDDQ_1V2_PIN},
+	 },
+
+	{"PG_DDR23_VDDQ_1V2", NODE_TYPE_CHECK, 0,
+	 {(unsigned long)PG_DDR23_VDDQ_1V2_PORT,
+	  (unsigned long)PG_DDR23_VDDQ_1V2_PIN},
+	 },
+
+	{"EN_DDR01_VTT_0V6", NODE_TYPE_ENABLE, 0,
 	 {(unsigned long)EN_DDR01_VTT_0V6_PORT,
 	  (unsigned long)EN_DDR01_VTT_0V6_PIN},
 	 },
 
-	{"P08_PG_B34", NODE_TYPE_ENABLE, 1000,
+	{"EN_DDR23_VTT_0V6", NODE_TYPE_ENABLE, 1000,
 	 {(unsigned long)EN_DDR23_VTT_0V6_PORT,
 	  (unsigned long)EN_DDR23_VTT_0V6_PIN},
 	 },
 
-	{"P08_PG_B56", NODE_TYPE_ENABLE, 30000,
+	{"EN_VQPS18", NODE_TYPE_ENABLE, 30000,
 	 {(unsigned long)EN_VQPS18_PORT, (unsigned long)EN_VQPS18_PIN},
 	 },
 
-	{"P08_PG_B78", NODE_TYPE_ENABLE, 1000,
+	{"SYS_RST_X_H", NODE_TYPE_ENABLE, 1000,
 	 {(unsigned long)SYS_RST_X_H_PORT, (unsigned long)SYS_RST_X_H_PIN},
 	 },
 
