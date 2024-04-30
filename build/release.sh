7a7065e58bbcf77f3ba63d53dc69f760cc8f1248
peng.wei <peng.wei@sophgo.com>
2024-04-30 10:55:09 +0800

    [SC7FP150] v0:Add packaging code
    Signed-off-by: peng.wei <peng.wei@sophgo.com>

--- build/release.sh
+++ build/release.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-list='sm5g se5lite sm5gd32 bm1684evb sa5 sc5h sc5plus sm5mini sc5pro bm1684xevb sc7pro sc7hp75 sg2042evb sg2042revb wolfclaw sg2042x4 sm7mini sm7mqy athena2evb athena2acp sm7g sm7m_mp_1_1 sm7m_mp_1_2' 
-
+list='sm5g se5lite sm5gd32 bm1684evb sa5 sc5h sc5plus sm5mini sc5pro bm1684xevb sc7pro sc7hp75 sc7fp150 sg2042evb sg2042revb wolfclaw sg2042x4 sm7mini sm7mqy athena2evb athena2acp sm7g sm7m_mp_1_1 sm7m_mp_1_2' 
 top="$PWD/.."
 
 unset rm
@@ -99,6 +98,10 @@ function version()
         dir="$top/SC7HP75"
         key_word='MCU_SW_VER'
         ;;
+    sc7fp150)
+	dir="$top/SC7FP150"
+	key_word='MCU_SW_VER'
+	;;
     sg2042evb)
         dir="$top/SG2042EVB"
         key_word='MCU_SW_VER'
