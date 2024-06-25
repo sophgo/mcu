da434e6b3b8bb360725e7a31ccade03d33ead802
chunzhi.lin <chunzhi.lin@sophgo.com>
2024-06-25 11:26:37 +0800

    SG2042X4(5):fix the bug that caused MCU to hang after multiple times soft reboot
    Signed-off-by: chunzhi.lin <chunzhi.lin@sophgo.com>

--- SG2042X4/slave.c
+++ SG2042X4/slave.c
@@ -15,15 +15,23 @@ static struct i2c_slave_ctx i2c2_slave_ctx;
 #define I2C2_OA2		0x68
 #define I2C2_OA2M		3
 
+static int slave_init_time = 1;
+
 void slave_init(void)
 {
 	i2c_slave_init(&i2c2_slave_ctx, (void *)I2C2, I2C2_OA1, I2C2_OA2, I2C2_OA2M);
-	mcu_init(&i2c2_slave_ctx);
-	eeprom_init(&i2c2_slave_ctx);
-	nct218_init(&i2c2_slave_ctx);
-	wdt_init(&i2c2_slave_ctx);
+
+	if (slave_init_time > 0) {
+		mcu_init(&i2c2_slave_ctx);
+		eeprom_init(&i2c2_slave_ctx);
+		nct218_init(&i2c2_slave_ctx);
+		wdt_init(&i2c2_slave_ctx);
+	}
+
 	i2c_slave_start(&i2c2_slave_ctx);
 	nvic_irq_enable(I2C2_EV_IRQn, 0, 0);
+
+	slave_init_time = 0;
 }
 
 /* replace default isr */
