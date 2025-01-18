9b4378847e1028bdc999dcc42c5473585adff448
chengjun.li <chengjun.li@sophgo.com>
2025-01-18 17:34:32 +0800

    sc11fp300(v17):adjust power seq

--- SC11FP300/board_power.c
+++ SC11FP300/board_power.c
@@ -13,17 +13,27 @@ void chip_deassert_n_off(void);
 
 struct power_node const board_power_nodes[28] = {
 
-	{"CHIP_DEASSERT_N", NODE_TYPE_FUNCTION, 50000,
+	{"CHIP_DEASSERT_N", NODE_TYPE_FUNCTION, 0,
 	 {(unsigned long)chip_deassert_n_on,
 	  (unsigned long)chip_deassert_n_off},
 	 },
 
-	{"EN_VDD5V", NODE_TYPE_ENABLE, 0,
+	{"EN_VDD_3V3", NODE_TYPE_ENABLE, 0,
+	 {(unsigned long)EN_VDD_3V3_PORT, (unsigned long)EN_VDD_3V3_PIN},
+	 },
+
+	{"EN_VDD5V", NODE_TYPE_ENABLE, 12800,
 	 {(unsigned long)EN_VDD_5V_PORT, (unsigned long)EN_VDD_5V_PIN},
 	 },
 
-	{"EN_VDD_3V3", NODE_TYPE_ENABLE, 100000,
-	 {(unsigned long)EN_VDD_3V3_PORT, (unsigned long)EN_VDD_3V3_PIN},
+	{"EN_DDR_VDD_BM0", NODE_TYPE_ENABLE, 0,
+	 {(unsigned long)EN_DDR_VDD_BM0_PORT,
+	  (unsigned long)EN_DDR_VDD_BM0_PIN},
+	 },
+
+	{"EN_DDR_VDD_BM1", NODE_TYPE_ENABLE, 18000,
+	 {(unsigned long)EN_DDR_VDD_BM1_PORT,
+	  (unsigned long)EN_DDR_VDD_BM1_PIN},
 	 },
 
 	{"EN_VDDC_B0", NODE_TYPE_ENABLE, 0,
@@ -38,7 +48,7 @@ struct power_node const board_power_nodes[28] = {
 	 {(unsigned long)EN_VDDR_BM0_PORT, (unsigned long)EN_VDDR_BM0_PIN},
 	 },
 
-	{"EN_VDDR_B1", NODE_TYPE_ENABLE, 0,
+	{"EN_VDDR_B1", NODE_TYPE_ENABLE, 1000,
 	 {(unsigned long)EN_VDDR_BM1_PORT, (unsigned long)EN_VDDR_BM1_PIN},
 	 },
 
@@ -47,29 +57,27 @@ struct power_node const board_power_nodes[28] = {
 	  (unsigned long)EN_VDD_PLL_BM0_PIN},
 	 },
 
-	{"EN_VDD_PLL_BM1", NODE_TYPE_ENABLE, 0,
+	{"EN_VDD_PLL_BM1", NODE_TYPE_ENABLE, 3200,
 	 {(unsigned long)EN_VDD_PLL_BM1_PORT,
 	  (unsigned long)EN_VDD_PLL_BM1_PIN},
 	 },
 
-	{"EN_DDR_VDD_BM0", NODE_TYPE_ENABLE, 0,
-	 {(unsigned long)EN_DDR_VDD_BM0_PORT,
-	  (unsigned long)EN_DDR_VDD_BM0_PIN},
+	{"EN_VDD_1V8", NODE_TYPE_ENABLE, 100,
+	 {(unsigned long)EN_VDD_1V8_PORT, (unsigned long)EN_VDD_1V8_PIN},
 	 },
 
-	{"EN_DDR_VDD_BM1", NODE_TYPE_ENABLE, 0,
-	 {(unsigned long)EN_DDR_VDD_BM1_PORT,
-	  (unsigned long)EN_DDR_VDD_BM1_PIN},
+	{"EN_VDD_1V2", NODE_TYPE_ENABLE, 2700,
+	 {(unsigned long)EN_VDD_1V2_PORT, (unsigned long)EN_VDD_1V2_PIN},
 	 },
 
-	{"EN_VDD_1V2", NODE_TYPE_ENABLE, 0,
-	 {(unsigned long)EN_VDD_1V2_PORT,
-	  (unsigned long)EN_VDD_1V2_PIN},
+	{"EN_DDR_VDD2H_BM0", NODE_TYPE_ENABLE, 0,
+	 {(unsigned long)EN_DDR_VDD2H_BM0_PORT,
+	  (unsigned long)EN_DDR_VDD2H_BM0_PIN},
 	 },
 
-	{"EN_VDD_1V8", NODE_TYPE_ENABLE, 100000,
-	 {(unsigned long)EN_VDD_1V8_PORT,
-	  (unsigned long)EN_VDD_1V8_PIN},
+	{"EN_DDR_VDD2H_BM1", NODE_TYPE_ENABLE, 2500,
+	 {(unsigned long)EN_DDR_VDD2H_BM1_PORT,
+	  (unsigned long)EN_DDR_VDD2H_BM1_PIN},
 	 },
 
 	{"EN_VPH_PCIE_BM0", NODE_TYPE_ENABLE, 0,
@@ -77,7 +85,7 @@ struct power_node const board_power_nodes[28] = {
 	  (unsigned long)EN_VPH_PCIE_BM0_PIN},
 	 },
 
-	{"EN_VPH_PCIE_BM1", NODE_TYPE_ENABLE, 0,
+	{"EN_VPH_PCIE_BM1", NODE_TYPE_ENABLE, 500,
 	 {(unsigned long)EN_VPH_PCIE_BM1_PORT,
 	  (unsigned long)EN_VPH_PCIE_BM1_PIN},
 	 },
@@ -87,19 +95,17 @@ struct power_node const board_power_nodes[28] = {
 	  (unsigned long)EN_VP_PCIE_BM0_PIN},
 	 },
 
-	{"EN_VP_PCIE_BM1", NODE_TYPE_ENABLE, 20000,
+	{"EN_VP_PCIE_BM1", NODE_TYPE_ENABLE, 6000,
 	 {(unsigned long)EN_VP_PCIE_BM1_PORT,
 	  (unsigned long)EN_VP_PCIE_BM1_PIN},
 	 },
 
-	{"EN_DDR_VAA_BM0", NODE_TYPE_ENABLE, 0,
-	 {(unsigned long)EN_DDR_VAA_BM0_PORT,
-	  (unsigned long)EN_DDR_VAA_BM0_PIN},
-	 },
-
-	{"EN_DDR_VAA_BM1", NODE_TYPE_ENABLE, 20000,
-	 {(unsigned long)EN_DDR_VAA_BM1_PORT,
-	  (unsigned long)EN_DDR_VAA_BM1_PIN},
+	/* The pin of DDR_VDD1_BM0 is different between the first version PCB(VER1) 
+	 * and subsequent PCBs, and can be simultaneously enabled for compatibility 
+	 * without using PC10 as the serial port */
+	{"EN_DDR_VDD1_BM0_VER1", NODE_TYPE_ENABLE, 0,
+	 {(unsigned long)EN_DDR_VDD1_BM0_VER0_PORT,
+	  (unsigned long)EN_DDR_VDD1_BM0_VER0_PIN},
 	 },
 
 	{"EN_DDR_VDD1_BM0", NODE_TYPE_ENABLE, 0,
@@ -107,27 +113,19 @@ struct power_node const board_power_nodes[28] = {
 	  (unsigned long)EN_DDR_VDD1_BM0_PIN},
 	 },
 
-	/* The pin of DDR_VDD1_BM0 is different between the first version PCB(VER1) 
-	 * and subsequent PCBs, and can be simultaneously enabled for compatibility 
-	 * without using PC10 as the serial port */
-	{"EN_DDR_VDD1_BM0_VER1", NODE_TYPE_ENABLE, 0,
-	 {(unsigned long)EN_DDR_VDD1_BM0_VER1_PORT,
-	  (unsigned long)EN_DDR_VDD1_BM0_VER1_PIN},
-	 },
-
-	{"EN_DDR_VDD1_BM1", NODE_TYPE_ENABLE, 20000,
+	{"EN_DDR_VDD1_BM1", NODE_TYPE_ENABLE, 5900,
 	 {(unsigned long)EN_DDR_VDD1_BM1_PORT,
 	  (unsigned long)EN_DDR_VDD1_BM1_PIN},
 	 },
 
-	{"EN_DDR_VDD2H_BM0", NODE_TYPE_ENABLE, 0,
-	 {(unsigned long)EN_DDR_VDD2H_BM0_PORT,
-	  (unsigned long)EN_DDR_VDD2H_BM0_PIN},
+	{"EN_DDR_VAA_BM0", NODE_TYPE_ENABLE, 0,
+	 {(unsigned long)EN_DDR_VAA_BM0_PORT,
+	  (unsigned long)EN_DDR_VAA_BM0_PIN},
 	 },
 
-	{"EN_DDR_VDD2H_BM1", NODE_TYPE_ENABLE, 20000,
-	 {(unsigned long)EN_DDR_VDD2H_BM1_PORT,
-	  (unsigned long)EN_DDR_VDD2H_BM1_PIN},
+	{"EN_DDR_VAA_BM1", NODE_TYPE_ENABLE, 7800,
+	 {(unsigned long)EN_DDR_VAA_BM1_PORT,
+	  (unsigned long)EN_DDR_VAA_BM1_PIN},
 	 },
 
 	{"EN_DDR_VDDQ_BM0", NODE_TYPE_ENABLE, 0,
@@ -135,16 +133,16 @@ struct power_node const board_power_nodes[28] = {
 	  (unsigned long)EN_DDR_VDDQ_BM0_PIN},
 	 },
 
-	{"EN_DDR_VDDQ_BM1", NODE_TYPE_ENABLE, 20000,
+	{"EN_DDR_VDDQ_BM1", NODE_TYPE_ENABLE, 12300,
 	 {(unsigned long)EN_DDR_VDDQ_BM1_PORT,
 	  (unsigned long)EN_DDR_VDDQ_BM1_PIN},
 	 },
 
-	{"CHIP_ASSERT_N", NODE_TYPE_FUNCTION, 0,
+	{"CHIP_ASSERT_N", NODE_TYPE_FUNCTION, 1000,
 	 {(unsigned long)chip_assert_n_on, (unsigned long)chip_assert_n_off},
 	 },
 
-	{"CHIP_DEASSERT_N", NODE_TYPE_FUNCTION, 30000,
+	{"CHIP_DEASSERT_N", NODE_TYPE_FUNCTION, 1000,
 	 {(unsigned long)chip_deassert_n_on,
 	  (unsigned long)chip_deassert_n_off},
 	 },
--- SC11FP300/common.h
+++ SC11FP300/common.h
@@ -4,7 +4,7 @@
 #include <pin.h>
 #include <gd32e50x.h>
 
-#define MCU_SW_VER	16
+#define MCU_SW_VER	17
 #define SOC_NUM		2
 
 #define false 0
