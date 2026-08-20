9e97d7288424089b393d4cf4490132ddfb348927
WynminMark <651292793@qq.com>
2026-08-20 16:52:29 +0800

    [fix] SM7M_MP_1_1 abnormal power-down issue caused by i2c reset (#83)
    Co-authored-by: peikai.zou <peikai.zou@sophgo.com>
    Co-authored-by: chengjun.li <chengjun.li@sophgo.com>

--- SM7M_MP_1_1/i2c-slaves/ct7451.c
+++ SM7M_MP_1_1/i2c-slaves/ct7451.c
@@ -25,6 +25,11 @@
 
 #define CT7451_COLLECT_INTERVAL		2000
 #define CT7451_OVER_TEMP_MAX		5
+/* consecutive I2C read failures before temperatures are cleared to 0.
+ * A reading of 0 never trips the over-temp guard (local_temp > critical_temp),
+ * so a stuck/unreachable sensor does not falsely power off the board.
+ * sample interval is 2s, so 5 failures ~= 10s */
+#define CT7451_READ_FAIL_MAX		5
 
 #define I2C				I2C2
 #define SMBTO				1
@@ -52,6 +57,7 @@ static struct ct7451_ctx {
 	int local_temp, remote_temp;	/* Local and Remote temperature */
 	int critical;			/* Critical temp */
 	int over_temp;
+	int read_fail_cnt;		/* consecutive I2C read failures */
 	volatile uint8_t *read_ptr, *write_ptr;
 	struct ct7451_reg map[CT7451_REG_MAX];
 } ct7451_ctx = {
@@ -190,42 +196,77 @@ static struct i2c_slave_op tmp451_slave = {
 
 static unsigned long last_time;
 
-static void ct7451_update_temp(void)
+static int ct7451_update_temp(void)
 {
 	uint8_t tmp;
+	int ret;
+	int fail = 0;
 
-	i2c_master_smbus_read_byte(I2C, CT7451_SLAVE_ADDR, SMBTO,
+	ret = i2c_master_smbus_read_byte(I2C, CT7451_SLAVE_ADDR, SMBTO,
 			CT7451_LT, &tmp);
-	ct7451_ctx.remote_temp = (int)tmp - 64;
+	if (ret) {
+		debug("ct7451 read remote temp timeout!(%d)\n", ret);
+		fail = 1;
+	} else {
+		debug("ct7451 read remote temp success! %d\n", tmp);
+		ct7451_ctx.remote_temp = (int)tmp - 64;
+	}
 
-	i2c_master_smbus_read_byte(I2C, CT7451_SLAVE_ADDR, SMBTO,
+	ret = i2c_master_smbus_read_byte(I2C, CT7451_SLAVE_ADDR, SMBTO,
 			CT7451_RT, &tmp);
-	ct7451_ctx.local_temp = (int)tmp - 64;
+	if (ret) {
+		debug("ct7451 read local temp fail!(%d)\n", ret);
+		fail = 1;
+	} else {
+		debug("ct7451 read local temp success! %d\n", tmp);
+		ct7451_ctx.local_temp = (int)tmp - 64;
+	}
+
+	if (fail) {
+		if (++ct7451_ctx.read_fail_cnt >= CT7451_READ_FAIL_MAX) {
+			/* sensor unreachable for too long: invalidate
+			 * temperatures. 0 never trips over-temp guard so
+			 * this avoids false power-off while exposing the
+			 * failure as an obvious bogus value. */
+			ct7451_ctx.remote_temp = 0;
+			ct7451_ctx.local_temp = 0;
+		}
+	} else {
+		ct7451_ctx.read_fail_cnt = 0;
+	}
 
 	set_soc_temp(ct7451_ctx.local_temp - 5);
 	set_board_temp(ct7451_ctx.remote_temp);
+
+	return fail ? -1 : 0;
 }
 
-void ct7451_get_temp(int *remote_temp, int *local_temp)
+int ct7451_get_temp(int *remote_temp, int *local_temp)
 {
-	ct7451_update_temp();
+	int ret = ct7451_update_temp();
 	*remote_temp = ct7451_ctx.remote_temp;
 	*local_temp = ct7451_ctx.local_temp - 5;
+	return ret;
 }
 
 static void ct7451_process(void)
 {
 	unsigned long current_time = tick_get();
 	int local_temp, remote_temp;
+	int ret;
 
 	if (current_time - last_time < CT7451_COLLECT_INTERVAL)
 		return;
 
 	last_time = current_time;
 
-	ct7451_get_temp(&remote_temp, &local_temp);
+	ret = ct7451_get_temp(&remote_temp, &local_temp);
 
-	if (get_needpoweron_satus() == 1) {
+	/* during read failure keep the last valid temperatures for the
+	 * over-temp guard (and 0 once failures persist), but skip the
+	 * power-on-back decision so a bogus 0 cannot falsely re-power
+	 * the board before it has cooled down. */
+	if (get_needpoweron_satus() == 1 && !ret) {
 		if (local_temp < get_repoweron_temp() && remote_temp < 80) {
 			chip_popd_reset_end();
 			clr_needpoweron();
@@ -258,17 +299,22 @@ static void ct7451_process(void)
 void ct7451_init(struct i2c_slave_ctx *i2c_slave_ctx)
 {
 	uint8_t tmp;
+	int ret;
 
 	/* Enable smbus timeout */
-	i2c_master_smbus_read_byte(I2C, CT7451_SLAVE_ADDR, SMBTO,
+	ret = i2c_master_smbus_read_byte(I2C, CT7451_SLAVE_ADDR, SMBTO,
 				   CT7451_ALERT, &tmp);
+	if (ret)
+		debug("ct7451_init read alert reg fail!(%d)\n", ret);
 	tmp |= CT7451_SMBTO_MASK;
 	i2c_master_smbus_write_byte(I2C, CT7451_SLAVE_ADDR,
 				    SMBTO, CT7451_ALERT, tmp);
 
 	/* Enable extended mode */
-	i2c_master_smbus_read_byte(I2C, CT7451_SLAVE_ADDR, SMBTO,
+	ret = i2c_master_smbus_read_byte(I2C, CT7451_SLAVE_ADDR, SMBTO,
 				   CT7451_CONFIG_RD_ADDR, &tmp);
+	if (ret)
+		debug("ct7451_init read config reg fail!(%d)\n", ret);
 	tmp |= CT7451_RANGE_MASK;
 	i2c_master_smbus_write_byte(I2C, CT7451_SLAVE_ADDR, SMBTO,
 				    CT7451_CONFIG_WR_ADDR, tmp);
--- SM7M_MP_1_1/i2c-slaves/ct7451.h
+++ SM7M_MP_1_1/i2c-slaves/ct7451.h
@@ -9,7 +9,7 @@
 #define __CT7451_H__
 
 void ct7451_init(struct i2c_slave_ctx *i2c_slave_ctx);
-void ct7451_get_temp(int *remote, int *local);
+int ct7451_get_temp(int *remote, int *local);
 void ct7451_process(void);
 
 #endif /* __CT7451_H__ */
